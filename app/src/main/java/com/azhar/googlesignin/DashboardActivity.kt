ee846bf7e714d1cee5c82b7f21b4580b813af1ec
MD. Azharul Islam <36479711+azharcse14@users.noreply.github.com>
2021-11-24 23:26:28 +0600

    Initial commit

---
+++ app/src/main/java/com/azhar/googlesignin/DashboardActivity.kt
@@ -0,0 +1,44 @@
+package com.azhar.googlesignin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+
+class DashboardActivity : AppCompatActivity() {
+
+    // declare the GoogleSignInClient
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+
+    private val auth by lazy {
+        FirebaseAuth.getInstance()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+
+        val logout:Button = findViewById(R.id.logout)
+
+        // call requestIdToken as follows
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        logout.setOnClickListener {
+            mGoogleSignInClient.signOut().addOnCompleteListener {
+                val intent = Intent(this, MainActivity::class.java)
+                Toast.makeText(this, "Logging Out", Toast.LENGTH_SHORT).show()
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+}
